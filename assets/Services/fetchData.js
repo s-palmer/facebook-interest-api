1f48ce9e0e0968326f0e2ade5a55f4f5d6bbdd13
Sergei Palmer <zigpalmer@gmail.com>
2021-12-22 13:18:17 +0000

    Working API Fetch to display data on homepage

---
+++ assets/Services/fetchData.js
@@ -0,0 +1,19 @@
+const Apikey = process.env.NEXT_PUBLIC_FACEBOOK_API_KEY;
+const FB_GRAPH_URL = process.env.NEXT_PUBLIC_FACEBOOK_GRAPH_URL;
+
+export async function getServerSideProps() {
+  const fetchURL = `${FB_GRAPH_URL}${Apikey}&q=[Golf]`;
+  let response = await fetch(fetchURL);
+  let json = await response.json();
+  const interests = json.data;
+
+  if (!json) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { interests }, // will be passed to the page component as props
+  };
+}
